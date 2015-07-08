7ac1287dc6ec2593d31a18e16ba4f4665819ef0a
Dustin Oprea <myselfasunder@gmail.com>
2015-07-08 02:33:39 -0400

    We can now take the database config from the environment.

--- Assignment3/dns_messages_receiver/dmr/config/db.py
+++ Assignment3/dns_messages_receiver/dmr/config/db.py
@@ -1,3 +1,5 @@
-HOSTNAME = 'localhost'
-PORT = 28015
-DATABASE = 'dmr'
+import os
+
+HOSTNAME = os.environ.get('DMR_DB_HOSTNAME', 'localhost')
+PORT = int(os.environ.get('DMR_DB_PORT', '28015'))
+DATABASE = os.environ.get('DMR_DB_DATABASE_NAME', 'dmr')
